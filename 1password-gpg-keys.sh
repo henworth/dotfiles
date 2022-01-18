a77a5d012241af31128711fc500416923f6ab1c7
Mike Hennessy <henworth@henabytes.com>
2022-01-18 10:09:04 -0500

    feat: add 1password cli for gpg keys

---
+++ 1password-gpg-keys.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+key_signature=$1
+
+if !command -v op >/dev/null 2>&1; then 
+    wget -O /tmp/op.zip https://cache.agilebits.com/dist/1P/op/pkg/v1.12.4/op_linux_arm64_v1.12.4.zip
+    unzip /tmp/op.zip -d /tmp
+    mv /tmp/op ~/bin/
+fi
+
+gpg --list-key "$key_signature" >/dev/null 2>&1
+test $? -eq 0 && exit
+
+eval $(op signin personal)
+op get document "Personal GPG Public Key" --output ~/.gnupg/gpg-personal-public.gpg
+op get document "Personal GPG Private Key" --output ~/.gnupg/gpg-personal-private.gpg
+
+gpg --import ~/.gnupg/gpg-personal-public.gpg ~/.gnupg/gpg-personal-private.gpg
+gpg --edit-key "$key_signature" trust quit
